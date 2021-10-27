341e412a7e571366fdb3a8687f0ef741283baad3
Katya Katsy <kkatsy@ucdavis.edu>
2021-10-26 18:40:11 -0700

    working on scanner

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod scanner;
 use std::io::prelude::*;
 use std::fs::File;
 use crate::character_stream::CharStream;
+use crate::scanner::Scanner;
 
 
 fn main() {
@@ -21,18 +22,13 @@ fn main() {
 	// println!("line numer: {}", token.get_line_number());
 	// println!("char position: {}", token.get_char_pos());
 
-	// let mut file = File::open("example1.x").expect("Unable to open the file");
-	// let mut contents = String::new();
 	let mut stream = CharStream::new("example1.x");
-	let contents = stream.get_contents();
-	let char_vec = stream.get_char_vector();
-	let available: bool = stream.more_available();
-	println!("array size is : {}",stream.get_length());
-	println!("{}", stream.more_available());
-	println!("{}", stream.get_pos());
-	println!("{}", stream.get_next_char().unwrap());
-	println!("{}", stream.get_pos());
-	// for (pos, e) in char_vec.iter().enumerate() {
-	// 	println!("Element at position {}: {:?}", pos, e);
-	// }
+
+	let keywords = vec!["unsigned", "char", "short", "int", "long", "float", "double","while", "if", "return", "void", "main"];
+	let operators = vec!["(", ",", ")", "{", "}", "=", "==", "<", ">", "<=", ">=", "!=", "+", "-", "*", "/", ";"];
+
+	let scanner: Scanner = Scanner::new(stream, keywords, operators);
+	let kws: &Vec<String> = scanner.get_operators();
+	println!("{:?}", kws);
+	scanner.get_world();
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -1,4 +1,6 @@
+use crate::character_stream::CharStream;
 use crate::token::Token;
+
 
 // Due to the fact that there is a unary negation “-” operation in X, the scanning has one slight com-
 // plication. If a “-” sign is followed by digits, but preceded by an ID or constant, it is considered the
@@ -9,20 +11,46 @@ use crate::token::Token;
 
 // The Scanner struct should have a method named get_next_token() or something similar that when
 // called, will return the next token as read from the .x file.
+fn get_non_blank() {
+    println!(" Hello World.");
+}
+fn another_function() {
+    println!(" Hello World.");
+}
 
 pub struct Scanner {
     // open the .x file as specified on the command line and tokenize the
     // text of the files into operators, intConstants, floatConstants, keywords, and identifiers.
     // uses character stream struct to do this
+    keywords: Vec<String>,
+    operators: Vec<String>,
+    char_stream: CharStream,
+    cur_lexeme: String,
+    tokens: Vec<Token>,
 }
 
 impl Scanner {
 
-    pub fn new(f: &str) -> Scanner {
-
+    pub fn new(c_s: CharStream, k: Vec<&str>, op: Vec<&str>) -> Scanner {
+        Scanner {
+            keywords: k.iter().map(|s| s.to_string()).collect(),
+            operators: op.iter().map(|s| s.to_string()).collect(),
+            char_stream: c_s,
+            cur_lexeme: "".to_string(),
+            tokens: Vec::new()
+        }
     }
 
-    pub fn getNextToken(&self) -> Token {
+    pub fn get_keywords(&self) -> &Vec<String> {&self.keywords}
+    pub fn get_operators(&self) -> &Vec<String> {&self.operators}
+    pub fn get_world(&self) -> () {another_function()}
 
-    }
+    // pub fn get_next_token(&mut self) -> Token {
+    //     let next_char= self.char_stream.get_next_char();
+    //     while {
+    //         lex();
+    //         self.char_stream.more_available();
+    //     } {};
+    //     return Token
+    // }
 }
